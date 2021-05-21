07aea1b802c5da30609a006d7f5e1b1c10f6a9a2
Larsenik67 <ylachqar@gmail.com>
2021-05-21 10:13:31 +0200

    Init

--- js/index.js
+++
@@ -1,7 +0,0 @@
-<script type='text/javascript'>
-    document.getElementById('fullscreen-bg-video').addEventListener('ended', detectEnd, false);
-
-    function detectEnd(e) {
-        jQuery('.fullscreen-bg').fadeOut(1000,function(){jQuery(this).remove()})
-    }
-</script>
---
+++ js/intro.js
@@ -0,0 +1,20 @@
+var e = document.getElementById("myvid");
+e.style.opacity = 0;
+
+var vid = document.getElementById("myvid");
+vid.oncanplaythrough = function() {
+    setTimeout(function() {
+        var e = document.getElementById('myvid');
+        fade(e);
+    }, 0);
+};
+
+function fade(element) {
+    var op = 0;
+    var timer = setInterval(function() {
+        if (op >= 1) clearInterval(timer);
+        element.style.opacity = op;
+        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+        op += op * 0.1 || 0.1;
+    }, 25);
+}
